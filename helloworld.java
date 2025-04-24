eaee81db222cd24321e9f482654cf7c7e09b5ad5
iqraamatte <klsvppcs23iqramatte@gmail.com>
2025-04-24 17:41:55 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,9 @@
 // Your First Program
 
-class helloworld {
-    public static void main(String[] args) {
+class helloworld
+    {
+    public static void main(String[] args)
+    {
         System.out.println("Hello, World!"); 
     }
 }
